c757b253ab16fdffc7373abbcd6ee02288dbe5e0
waseem <waseemattar14@gmail.com>
2022-11-25 18:09:49 +0530

    Factorial code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void checkEvenOdd() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER A NUMBER - ");
+        int number = sc.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " IS EVEN.");
+        } else {
+            System.out.println(number + " IS ODD.");
+        }
+    }
+
+    public static void getFactorial() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER A NUMBER - ");
+        int number = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i < number; i++) {
+            fact *= i;
+        }
+        System.out.println("FACTORIAL OF " + number + " IS " + fact);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkEvenOdd();
+        getFactorial();
+
     }
 }
